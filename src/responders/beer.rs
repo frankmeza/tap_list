5b2b90a6bd1cf9c12e8d009f594aceaf9b500180
Frank Meza <frankmezapdx@gmail.com>
2020-02-10 22:40:59 -0800

    add get_beer_list in responders

--- src/responders/beer.rs
+++ src/responders/beer.rs
@@ -1,7 +1,13 @@
-use actix_web::{web, HttpRequest, HttpResponse, Responder};
-use std::time::{SystemTime, UNIX_EPOCH};
-use uuid::Uuid;
+use crate::{get_connection, handlers};
+
+use actix_web::{HttpResponse, Responder};
+// use actix_web::{web, HttpRequest, HttpResponse, Responder};
+// use std::time::{SystemTime, UNIX_EPOCH};
+// use uuid::Uuid;
 
 pub fn get_beer_list() -> impl Responder {
-    HttpResponse::Ok()
+    let conn = get_connection();
+    let beer_list = handlers::fetch_beer_list(conn);
+
+    HttpResponse::Ok().json(beer_list)
 }
