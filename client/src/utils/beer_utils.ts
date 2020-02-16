d2bd45383a3fad52c5088064bab204238c9eb34a
Frank Meza <frankmezapdx@gmail.com>
2020-02-15 22:16:54 -0800

    impl BeerRemote shape

--- client/src/utils/beer_utils.ts
+++ client/src/utils/beer_utils.ts
@@ -1,10 +1,10 @@
 import { Beer } from "core"
-import { BASE_URL } from "../remote"
+import { BASE_URL, BeerRemote } from "../remote"
 import { camelCaseObject } from "./transform"
 
 export async function getBeerList(): Promise<Beer[]> {
     const response = await fetch(`${BASE_URL}/beers`)
-    const beerList: Beer[] = await response.json()
+    const beerList: BeerRemote[] = await response.json()
 
     return beerList.map(beer => camelCaseObject(beer) as Beer)
 }
