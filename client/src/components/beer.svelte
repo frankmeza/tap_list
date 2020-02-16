a34fc02314e454105cfe09960b3c68b4601d82a8
Frank Meza <frankmezapdx@gmail.com>
2020-02-15 22:16:43 -0800

    add SortOrder component

--- client/src/components/beer.svelte
+++ client/src/components/beer.svelte
@@ -1,32 +1,23 @@
 <script lang="ts">
     import { defaultBeer } from "../core"
-    export let beer = defaultBeer
+    import SortOrderDot from "./sort_order.svelte"
 
+    export let beer = defaultBeer
     const { sortOrder } = beer
 </script>
 
 <div class="beer-container">
-    <div class="sort-order-dot">
-        {sortOrder}
-    </div>
+    <SortOrderDot sortOrder={sortOrder} />
     <p>{JSON.stringify(beer, null, 4)}</p>
 </div>
 
 <style>
     .beer-container {
-        /* display: grid;
-        grid-template-columns: 50% 50%; */
+        display: grid;
+        grid-template-columns: 10% 90%;
         border: solid 2px black;
         border-radius: 4px;
         margin-right: 4px;
         padding: 4px;
-    }
-
-    .sort-order-dot {
-        border: solid 2px black;
-        border-radius: 25%;
-        margin-right: 4px;
-        height: 50%;
-        width: 10%;
     }
 </style>
---
+++ client/src/components/sort_order.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let sortOrder = 0
+</script>
+
+<div class="sort-order-dot">
+    {sortOrder}
+</div>
+
+<style>
+    .sort-order-dot {
+        border: solid 2px black;
+        border-radius: 25%;
+        margin-right: 4px;
+        height: 50%;
+        width: 10%;
+    }
+</style>
