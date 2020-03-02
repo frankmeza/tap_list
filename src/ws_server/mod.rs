9f626f9c2c850cd998189fb60239cb425083ed16
Frank Meza <frankmezapdx@gmail.com>
2020-03-01 19:57:30 -0800

    comment out ws

--- src/ws_server/mod.rs
+++ src/ws_server/mod.rs
@@ -1,28 +1,28 @@
-use actix::{Actor, StreamHandler};
-use actix_web::{web, Error, HttpRequest, HttpResponse};
-use actix_web_actors::ws;
+// use actix::{Actor, StreamHandler};
+// use actix_web::{web, Error, HttpRequest, HttpResponse};
+// use actix_web_actors::ws;
 
-/// Define http actor
-struct WebSocket;
+// /// Define http actor
+// struct WebSocket;
 
-impl Actor for WebSocket {
-    type Context = ws::WebsocketContext<Self>;
-}
+// impl Actor for WebSocket {
+//     type Context = ws::WebsocketContext<Self>;
+// }
 
-/// Handler for ws::Message message
-impl StreamHandler<ws::Message, ws::ProtocolError> for WebSocket {
-    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
-        match msg {
-            ws::Message::Ping(msg) => ctx.pong(&msg),
-            ws::Message::Text(text) => ctx.text(text),
-            ws::Message::Binary(bin) => ctx.binary(bin),
-            _ => (),
-        }
-    }
-}
+// /// Handler for ws::Message message
+// impl StreamHandler<ws::Message, ws::ProtocolError> for WebSocket {
+//     fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+//         match msg {
+//             ws::Message::Ping(msg) => ctx.pong(&msg),
+//             ws::Message::Text(text) => ctx.text(text),
+//             ws::Message::Binary(bin) => ctx.binary(bin),
+//             _ => (),
+//         }
+//     }
+// }
 
-pub fn start(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(WebSocket {}, &req, stream);
-    println!("{:?}", resp);
-    resp
-}
+// pub fn start(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+//     let resp = ws::start(WebSocket {}, &req, stream);
+//     println!("{:?}", resp);
+//     resp
+// }
