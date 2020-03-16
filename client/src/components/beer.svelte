6b5b54e85cd938f8afcddc1b880032d47d172206
Frank Meza <frankmezapdx@gmail.com>
2020-03-15 17:14:48 -0700

    playing around with layour

--- client/src/components/beer.svelte
+++ client/src/components/beer.svelte
@@ -1,24 +1,64 @@
-<script lang="ts">
-    import { defaultBeer, Beer } from "../core"
-    import SortOrderDot from "./sort_order.svelte"
+<script>
+    import { defaultBeer } from "../core"
 
     export let beer = defaultBeer
-    const { sortOrder } = beer
+
+    const {
+        id = "",
+        sortOrder = 0,
+        name = "",
+        beerType = "",
+        abv = "",
+        ibu = "",
+        servingSize = "",
+        cost = "",
+        breweryName = "",
+        breweryCity = "",
+        breweryState = "",
+        breweryImgUrl = "",
+        kegId = "",
+        kegSize = 0,
+        kegAmountLeft = 0,
+        updatedTs = 0,
+        createdTs = 0,
+    } = beer
+
+    $: percentLeft = Math.round(kegSize / kegAmountLeft)
 </script>
 
+<!-- <pre>{JSON.stringify(beer, null, 4)}</pre> -->
 <div class="beer-container">
-    <SortOrderDot sortOrder={sortOrder} />
+    <div class="temp-border">
+        {sortOrder}
+    </div>
 
-    <p>{JSON.stringify(beer, null, 4)}</p>
+    <div class="temp-border">
+        {breweryImgUrl}
+    </div>
+
+    <div class="temp-border">
+        <p>{breweryName} {name}</p>
+        <p>{beerType}</p>
+
+        <p>{breweryCity}, {breweryState} ABV {abv}%</p>
+    </div>
+
+    <div class="temp-border">
+        <p>KEG AMOUNT LEFT: {percentLeft}%</p>
+    </div>
 </div>
 
 <style>
     .beer-container {
         display: grid;
-        grid-template-columns: 10% 90%;
+        grid-template-columns: 15% 15% 55% 15%;
         border: solid 2px black;
         border-radius: 4px;
         margin-right: 4px;
         padding: 4px;
+    }
+
+    .temp-border {
+        border: solid 2px black;
     }
 </style>
