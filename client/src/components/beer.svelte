537220be7e666dc8e7fd9d28eac1907898d0c53f
Frank Meza <frankmezapdx@gmail.com>
2020-03-20 15:53:16 -0700

    rm temp-border class and style

--- client/src/components/beer.svelte
+++ client/src/components/beer.svelte
@@ -25,25 +25,26 @@
     } = beer
 
     $: percentLeft = Math.round(kegSize / kegAmountLeft)
+    // TODO ROBOHASH https://robohash.org/ for image url -> create a hash from name, beerType, breweryName
 </script>
 
 <!-- <pre>{JSON.stringify(beer, null, 4)}</pre> -->
 <div class="beer-container">
-    <div class="temp-border">
+    <div class="section">
         {sortOrder}
     </div>
 
-    <div class="temp-border">
+    <div class="section">
         {breweryImgUrl}
     </div>
 
-    <div class="temp-border">
+    <div class="section">
         <p>{breweryName} {name}</p>
         <p>{beerType}</p>
         <p>{breweryCity}, {breweryState} ABV {abv}%</p>
     </div>
 
-    <div class="temp-border">
+    <div class="section">
         <p>KEG AMOUNT LEFT: {percentLeft}%</p>
     </div>
 </div>
@@ -53,19 +54,14 @@
         display: grid;
         grid-template-columns: 15% 15% 55% 15%;
 
-        border: solid 2px black;
-        border-radius: 4px;
-
         margin-right: 4px;
         padding: 4px;
 
-        /* background: rgb(238,174,202);
-        background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%); */
         background: rgb(34,193,195);
         background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
     }
 
-    .temp-border {
-        border: solid 2px black;
+    .section {
+        padding: 1em;
     }
 </style>
