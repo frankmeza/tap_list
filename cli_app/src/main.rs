40ccedb77dc0b42d69251d89ae35f8e2a6fbd2a9
Frank Meza <frankmezapdx@gmail.com>
2020-04-05 16:13:39 -0700

    echo user input within loop

--- cli_app/src/main.rs
+++ cli_app/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
+mod ws_client;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n Choose a beer to pour:\n");
+
+    loop {
+        // creates receiver variable for beer to pour
+        let mut number_choice = String::new();
+        io::stdin().read_line(&mut number_choice).unwrap().to_string();
+
+        let beer_choice = number_choice.as_str().trim();
+        println!("You chose {}", beer_choice);
+    }
 }
